19dc6fc5b7066c84669956234ddd39f4f712f2f5
Sarah Heavner <sheavner@gmail.com>
2020-09-29 18:12:49 -0600

    created csv file path for PyBank and PyPoll

---
+++ PyBank/Main.py
@@ -0,0 +1,16 @@
+#Import os module to allow files across different operating systems
+import os
+
+#Import module for reading csv files
+import csv
+
+#Create path to read csv file
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+
+#Open csv file
+with open(csvpath) as csv file:
+
+    #Set csvreader with comma as delimiter
+    csvreader = csv.reader(csvfile, delimiter=",")
+
