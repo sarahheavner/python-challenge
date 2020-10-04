34863be2869502314e068050d25f891b34870e96
Sarah Heavner <sheavner@gmail.com>
2020-10-04 13:30:54 -0600

    calculated monthly change in list, average monthly change, identified min and max change amounts. still need to print min/max month in statement

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -4,7 +4,6 @@ import os
 #Import csv module to read CSV file
 import csv
 
-
 #Create path to CSV file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -15,34 +14,44 @@ with open(csvpath, newline='') as csvfile:
     #Skip first row (header)
     csv_header = next(csvreader)
 
-    #Define Variables
+    #Create Lists
     date = []
     profit = []
     monthly_change = []
 
-    #Set starting values at 0
-    #month_count = 0
-    #monthly_change = 0
-
+    #Set previous_revenue value as first profit/loss value in data set
+    previous_revenue = 867884
+    
+    #Loop through and append lists
     for row in csvreader:
-        #Counts number of rows excluding header
-        #month_count += 1
-        #print(month_count)
-
+        #Append lists through loop
         date.append(row[0])
-
         profit.append(int(row[1]))
+        #Calculate monthly change in revenue
+        monthly_change.append(int(row[1]) - previous_revenue)
+        #Reset previous_revenue value for next loop calculation
+        previous_revenue = int(row[1])
 
-
-
-
+        
+    #Count number of rows 
     total_months=len(date)
+    #Calculate total profit
     total_profit=sum(profit)
-
+    #Calculate average montly change
+    total_monthly_change = sum(monthly_change)
+    average_change=round((total_monthly_change)/(int(total_months)-1),2)
+    #Identify greatest monthly increase and decrease
+    greatest_increase=max(monthly_change)    
+    greatest_decrease=min(monthly_change)
+    
+    #print financial analysis
     print('Financial Analysis')
     print(f'----------------------')
     print(f'Total Months: {total_months}')
     print(f'Total: {total_profit}')
+    print(f'Average Change: {average_change}')
+    print(f'Greatest Increase in Profits: {greatest_increase}')
+    print(f'Greatest Decrease in Profits: {greatest_decrease}')
    
     
         
